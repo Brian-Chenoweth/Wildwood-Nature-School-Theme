d7f03c3a820e90d9e97ddcb3f3786ce2e04063ea
brianchenoweth <brianmchenoweth@gmail.com>
2019-08-05 15:53:38 -0700

    Update to homepage programs shortcode

--- shortcodes/homepage-programs.php
+++ shortcodes/homepage-programs.php
@@ -5,26 +5,20 @@
     <div id="homepage-programs">
 
         <div class="program-1">
-            <a href="" class="program-link">Self-guided Exploration</a>
+            <a href="" class="program-link">Prechool</a>
             <img src="/wp-content/themes/wildwoodnature/img/kids-tree.jpg" alt="">
             <p>This should contain a small description of the program. Lorem ipsum dolor sit amet, debet delicata complectitur sea at.</p>
         </div>
 
         <div class="program-2">
-            <a href="" class="program-link">Teacher-led Group Time</a>
+            <a href="" class="program-link">Tiny Seeds Parent/Child Toddler Class</a>
             <img src="/wp-content/themes/wildwoodnature/img/kids-arms.png" alt="">
             <p>This should contain a small description of the program. Lorem ipsum dolor sit amet, debet delicata complectitur sea at.</p>
         </div>
 
         <div class="program-3">
-            <a href="" class="program-link">Individualized Instruction</a>
+            <a href="" class="program-link">Consulting</a>
             <img src="/wp-content/themes/wildwoodnature/img/lead_720_405.jpg" alt="">
-            <p>This should contain a small description of the program. Lorem ipsum dolor sit amet, debet delicata complectitur sea at.</p>
-        </div>
-
-        <div class="program-4">
-            <a href="" class="program-link">Outdoor Play</a>
-            <img src="/wp-content/themes/wildwoodnature/img/downloadsdsdsd.png" alt="">
             <p>This should contain a small description of the program. Lorem ipsum dolor sit amet, debet delicata complectitur sea at.</p>
         </div>
 

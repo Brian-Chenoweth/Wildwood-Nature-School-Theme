2e9a93a2ca26c94e084cc42dac5704dbc5d8a706
brianchenoweth <brianmchenoweth@gmail.com>
2019-08-05 17:25:14 -0700

    Updates to homepage-programs

--- shortcodes/homepage-programs.php
+++ shortcodes/homepage-programs.php
@@ -1,29 +1,29 @@
 <div id="homepage-programs-wrap">
 
-    <h2>A Day in the Life</h2>
+    <h2>Learn what program works for you.</h2>
 
     <div id="homepage-programs">
 
         <div class="program-1">
-            <a href="" class="program-link">Prechool</a>
-            <img src="/wp-content/themes/wildwoodnature/img/kids-tree.jpg" alt="">
-            <p>This should contain a small description of the program. Lorem ipsum dolor sit amet, debet delicata complectitur sea at.</p>
+            <a href="/programs/toddlers/" class="program-link">Tiny Seeds<br><span>Parent/Child Toddler Class</span></a>
+            <img src="/wp-content/themes/wildwoodnature/img/tiny-seeds-home.jpg" alt="">
+            <p>For children ages 1-3 and their caregivers. Parents learn and grow with their children.</p>
         </div>
 
         <div class="program-2">
-            <a href="" class="program-link">Tiny Seeds Parent/Child Toddler Class</a>
-            <img src="/wp-content/themes/wildwoodnature/img/kids-arms.png" alt="">
-            <p>This should contain a small description of the program. Lorem ipsum dolor sit amet, debet delicata complectitur sea at.</p>
+            <a href="/programs/preschool/" class="program-link">Prechool</a>
+            <img src="/wp-content/themes/wildwoodnature/img/preschool-home.jpg" alt="">
+            <p>For children ages 3 to 5 year olds who are fully toilet-trained.</p>
         </div>
 
         <div class="program-3">
-            <a href="" class="program-link">Consulting</a>
+            <a href="/programs/consulting/" class="program-link">Consulting<br><span>Bring nature to your program/children</span></a>
             <img src="/wp-content/themes/wildwoodnature/img/lead_720_405.jpg" alt="">
-            <p>This should contain a small description of the program. Lorem ipsum dolor sit amet, debet delicata complectitur sea at.</p>
+            <p>A small description of your consulting program should go here.</p>
         </div>
 
     </div>
 
-<a class="btn" href="/about-us/">Learn More</a>
+<a class="btn" href="/programs/">Learn More</a>
 
 </div>

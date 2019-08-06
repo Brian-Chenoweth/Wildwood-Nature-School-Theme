782f15aa312ad0ee466e83930c8ba781da451236
brianchenoweth <brianmchenoweth@gmail.com>
2019-08-05 21:03:55 -0700

    homepage programs update

--- shortcodes/homepage-programs.php
+++ shortcodes/homepage-programs.php
@@ -13,13 +13,13 @@
         <div class="program-2">
             <a href="/programs/preschool/" class="program-link">Preschool</a>
             <img src="/wp-content/themes/wildwoodnature/img/preschool-home.jpg" alt="">
-            <p>For children ages 3 to 5 year olds who are fully toilet-trained.</p>
+            <p>For children ages 3 to 5, who are fully toilet-trained.</p>
         </div>
 
         <div class="program-3">
-            <a href="/programs/consulting/" class="program-link">Consulting<br><span>Bring nature to your program/children</span></a>
+            <a href="/programs/consulting/" class="program-link">Consulting<br><span>Bring nature to your program/children.</span></a>
             <img src="/wp-content/themes/wildwoodnature/img/lead_720_405.jpg" alt="">
-            <p>A small description of your consulting program should go here.</p>
+            <p>Workshops, staff development, parent discussions and program consulting.</p>
         </div>
 
     </div>
